f5ec68fe6a6c3f56e74265e74e76496f1d3158af
Roberto Gaudenzi <robgaudenzi@gmail.com>
2017-10-21 18:27:21 +0200

    Main class to compute paths likelihood completed

--- src/main/LikelihoodMain.java
+++ src/main/LikelihoodMain.java
@@ -7,18 +7,16 @@ import likelihood.ComputeSL;
 import utils.Constants;
 import utils.JacksonHAGUtils;
 import utils.JacksonPathUtils;
+import utils.JacksonSubgraphUtils;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Roberto Gaudenzi on 15/10/17.
  */
 public class LikelihoodMain {
 
-    private static Map<String, IAttackPath> paths;
+    private static Map<String, IAttackPath> pathsMap;
 
     public static void main(String[] args){
         String dataFolderPath = Constants.getDataHome();
@@ -26,36 +24,45 @@ public class LikelihoodMain {
 
         String attackGraphFile = attackGraphName + ".json";
         String attackPathsFile = attackGraphName + "_paths.json";
+        String subgraphsFile   = attackGraphName + "_subgraphs.json";
 
         //Load hypergraph
         IHyperGraph graph = JacksonHAGUtils.loadCompressedHAG(dataFolderPath, attackGraphFile);
 
-        //Load the related paths
-        paths = JacksonPathUtils.loadPaths(dataFolderPath, attackPathsFile);
+        //Load the related pathsMap
+        pathsMap = JacksonPathUtils.loadPaths(dataFolderPath, attackPathsFile);
 
-        //TODO Load the subgraphs associated with each target node
-        Map<String, Set<IHostNode>> subgraphsMap = null;
+        Map<String, Set<IHostNode>> subgraphsMap = JacksonSubgraphUtils.loadSubgraphs(dataFolderPath, subgraphsFile, graph.getHostNodes());
 
-        Map<String, Set<IAttackPath>> pathsMap = filterPathsByTarget(paths.values());
-        //For each target T, call ComputeSL and obtain the Success Likelihood for all the paths that lead to T itself.
-        for(String targetID : pathsMap.keySet()){
-            Map<String, Float> pathsSL = ComputeSL.execute(graph, subgraphsMap.get(targetID), pathsMap.get(targetID), graph.getNode(targetID));
+        Map<String, Set<IAttackPath>> pathsByTarget = labelPathsByTarget(pathsMap.values());
+        //For each target T, call ComputeSL and obtain the Success Likelihood for all the pathsMap that lead to T itself.
+        for(String targetID : pathsByTarget.keySet()){
+            Map<String, Float> pathsSL = ComputeSL.execute(graph, subgraphsMap.get(targetID), pathsByTarget.get(targetID), graph.getNode(targetID));
 
             //Store the SL value into the associated path
             setSL(pathsSL);
         }
+
+        // Finally, store the pathsMap with the updated SL value in a file
+        JacksonPathUtils.storePaths(attackGraphName, pathsMap.values(), dataFolderPath, attackPathsFile);
     }
 
     private static void setSL(Map<String, Float> pathsSL){
         for(String pathID: pathsSL.keySet()){
-            paths.get(pathID).setLikelihood(pathsSL.get(pathID));
+            pathsMap.get(pathID).setLikelihood(pathsSL.get(pathID));
         }
     }
 
-    private static Map<String, Set<IAttackPath>> filterPathsByTarget(Collection<IAttackPath> paths){
+    private static Map<String, Set<IAttackPath>> labelPathsByTarget(Collection<IAttackPath> paths){
         Map<String, Set<IAttackPath>> pathsMap = new HashMap<>();
 
-        //TODO For each path, look at the Head of the last edge: this is the target.
+        for(IAttackPath path : paths){
+            String targetID = path.getTargetID();
+            if(!pathsMap.keySet().contains(targetID)){
+                pathsMap.put(targetID, new HashSet<>());
+            }
+            pathsMap.get(path.getTargetID()).add(path);
+        }
 
         return pathsMap;
     }
